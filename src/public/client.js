ce56d192ffb792af6069dc9474e6c23763c99d94
javiermolsanz <javier.molinasanz@vonage.com>
2021-04-13 18:07:35 +0100

    adding go back button

--- src/public/client.js
+++ src/public/client.js
@@ -50,10 +50,13 @@ const App = data => {
     <main>
       <section>
           <h3>Put things on the page!</h3>
-          <p>
+          <div>
+          <button onclick="chooseAgain()">
+          Go back
+        </button>
           ${displayRoverData(data.photos[0])}
-          
-          </p>
+          ${displayRoverPictures(data.photos)}
+          </div>
           
       </section>
   </main>
@@ -65,19 +68,14 @@ const createUI = async rover => {
   const data = await roverData(rover);
   //const data = "foo";
   root.innerHTML = App(data);
-
-  // const render = async (root, state) => {
-  //     root.innerHTML = App(state);
-  //   };
-  // const data = await roverData(rover);
-  // displayRoverData(data.photos[0]);
-  // displayRoverPictures(data.photos);
-  //constroverData(rover).then(d => displayRover(d.photos));
 };
-// listening for load event because page should load before any JS is called
-// window.addEventListener("load", () => {
-//   render(root, store);
-// });
+
+const chooseAgain = () => {
+  mainDiv.style.display = "flex";
+  mainDiv.setAttribute("class", "main");
+  root.innerHTML = ``;
+  root.style.display = "none";
+};
 
 const roverData = async rover => {
   const url = "/roverData";
@@ -103,61 +101,18 @@ const displayRoverData = roverData => {
 };
 
 const displayRoverPictures = roverData => {
+  let images = ``;
   roverData.map(rover => {
-    return `<img src="${rover.img_src}" height="350px" width="100%" />`;
+    images += `<img src="${rover.img_src}" height="350px" width="50%" />`;
   });
+  return images;
 };
 // ------------------------------------------------------  COMPONENTS
 
 // Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = name => {
-  if (name) {
-    return `
-            <h1>Welcome, ${name}!</h1>
-        `;
-  }
-
-  return `
-        <h1>Hello!</h1>
-    `;
-};
 
 // Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = apod => {
-  // If image does not already exist, or it is not from today -- request it again
-  const today = new Date();
-  const photodate = new Date(apod.date);
-  console.log(photodate.getDate(), today.getDate());
-
-  console.log(photodate.getDate() === today.getDate());
-  if (!apod || apod.date === today.getDate()) {
-    getImageOfTheDay(store);
-  }
-
-  // check if the photo of the day is actually type video!
-  if (apod.media_type === "video") {
-    return `
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `;
-  } else {
-    return `
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `;
-  }
-};
 
 // ------------------------------------------------------  API CALLS
 
 // Example API call
-const getImageOfTheDay = state => {
-  let { apod } = state;
-
-  fetch(`http://localhost:3000/apod`)
-    .then(res => res.json())
-    .then(apod => updateStore(store, { apod }));
-
-  return data;
-};
